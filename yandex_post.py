34e5e91f43ae63701215daa68f57346bdbb0fc66
Gusar82 <rusgusa82@gmail.com>
2022-04-20 22:28:25 +0500

    task No.2 completed

--- yandex_post.py
+++ yandex_post.py
@@ -1,16 +1,29 @@
+import requests
+
+
 class YaUploader:
     def __init__(self, token: str):
         self.token = token
 
-    def upload(self, file_path: str):
-        """Метод загружает файлы по списку file_list на яндекс диск"""
-        # Тут ваша логика
-        # Функция может ничего не возвращать
+    def _get_href(self, file_path):
+        upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token)
+        }
+        params = {'path': file_path, 'overwrite': 'true'}
+        response = requests.get(upload_url, headers=headers, params=params)
+        return response.json()["href"]
+
+    def upload(self, file_path):
+        response = requests.put(self._get_href(file_path), data=open(file_path, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            print("Succes")
 
 
 if __name__ == '__main__':
-    # Получить путь к загружаемому файлу и токен от пользователя
-    path_to_file = ...
-    token = ...
+    path_to_file = "test.txt"
+    token = "TOKEN"
     uploader = YaUploader(token)
-    result = uploader.upload(path_to_file)
+    uploader.upload(path_to_file)
